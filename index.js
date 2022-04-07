48f50f13eadcef5d08a1aaceb22af45322e28829
Okuhle Mhambi <bukhosimhambi@gmail.com>
2022-04-07 12:06:39 +0200

    Added an add Item Button

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4017;
 
+// enable the req.body object - to allow us to use HTML forms
+// when doing post requests
+// put this before you declare any routes
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // API routes to be added here
 app.get('/api/garments', function(req, res){
 
@@ -44,14 +51,55 @@ app.get('/api/garments', function(req, res){
         });
     });
     
-    
-
 	// note that this route just send JSON data to the browser
 	// there is no template
 	res.json({ 
 		garments : filteredGarments
 	});
 });
+
+
+app.post('/api/garments', (req, res) => {
+
+	// get the fields send in from req.body
+	const {
+		description,
+		img,
+		gender,
+		season,
+		price
+	} = req.body;
+
+	// add some validation to see if all the fields are there.
+	// only 3 fields are made mandatory here
+	// you can change that
+
+	if (!description || !img || !price) {
+		res.json({
+			status: 'error',
+			message: 'Required data not supplied',
+		});
+	} else {
+
+		// you can check for duplicates here using garments.find
+		
+		// add a new entry into the garments list
+		garments.push({
+			description,
+			img,
+			gender,
+			season,
+			price
+		});
+
+		res.json({
+			status: 'success',
+			message: 'New garment added.',
+		});
+	}
+
+});
+
 
 app.listen(PORT, function() {
 	console.log(`App started on port ${PORT}`)
